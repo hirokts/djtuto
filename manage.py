e34ab93fe7bdc1239de617fdf8fb1bf6b56f8843
hirokts <hkchatar@outlook.jp>
2017-01-01 08:02:57 +0000

    Django test app

---
+++ manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.7
+        raise
+    execute_from_command_line(sys.argv)
